23fb5e35716366aade66443935ccccd5731418aa
dipanc1 <dipanchhabra@gmail.com>
2025-08-13 11:10:32 +0530

    fix: update backend URL to use production environment

--- mean-rejection-email-generator/src/config.js
+++ mean-rejection-email-generator/src/config.js
@@ -1,3 +1,4 @@
-const backend_url = "http://localhost:3001";
+// const backend_url = "http://localhost:3001";
+const backend_url = "https://mean-rejection-email-generator.onrender.com";
 
 export default backend_url;
